1b27437563aba53ba4a15d0d1b92c9a8d65066b7
michal.bandyszak <michal.bandyszak@globallogic.com>
2023-03-22 16:17:20 +0100

    changed fetch to axios

--- src/API/api.js
+++ src/API/api.js
@@ -1,204 +1,88 @@
+import axios from 'axios';
 
 export function login(username, password) {
-  const params = new URLSearchParams({username, password});
-  return fetch(`http://localhost:8088/login?${params.toString()}`)
+  const params = {username, password};
+  return axios.post('http://localhost:8088/login', params)
     .then((response) => {
-      if(!response.ok) {
-        throw new Error("HTTP status " + response.status);
-      }
-      return response.json();
-    })
-}
-
-export function getShiftById(token, shiftId) {
-  const params = new URLSearchParams({token, shiftId});
-  return fetch(`http://localhost:8088/shift?${params.toString()}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("HTTP status " + response.status);
-      }
-      return response.json();
-    })
-}
-
-
-export function addShift(token, date, startHour, endHour, isAvailable, workerId) {
-  const data = new FormData();
-  data.append('token', token);
-  data.append('date', date);
-  data.append('startHour', startHour);
-  data.append('endHour', endHour);
-  data.append('isAvailable', isAvailable);
-  data.append('workerId', workerId);
-
-  return fetch('http://localhost:8088/shift', {
-    method: 'POST',
-    body: data
-  })
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("HTTP status " + response.status);
-    }
-    return response.json();
-  });
-}
-
-export function updateWorkerShift(token, startHour, endHour, isAvailable, shiftId, workerId) {
-  const formData = new FormData();
-  formData.append('startHour', startHour);
-  formData.append('endHour', endHour);
-  formData.append('isAvailable', isAvailable);
-  formData.append('workerId', workerId);
-  
-  return fetch(`http://localhost:8088/shift?${shiftId}`, {
-    method: 'PUT',
-    headers: {
-      'Authorization': `Bearer ${token}`
-    },
-    body: formData
-  })
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("HTTP status " + response.status);
-    }
-    return response.json();
-  });
-}
-
-export function deleteWorkerShift(token, shiftId, workerId) {
-  const formData = new FormData();
-  formData.append('shiftId', shiftId);
-  formData.append('workerId', workerId);
-  return fetch(`http://localhost:8088/shift?${shiftId, workerId}`, {
-    method: 'DELETE',
-    headers: {
-      'Authorization': `Bearer ${token}`
-    },
-    body: formData
-  })
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("HTTP status " + response.status);
-    }
-    return response.json();
-  });
-}
-
-
-//Function to get all shifts from server -> main function 
-export function getAllShifts() {
-  return fetch('http://localhost:8088/shift/all')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("HTTP status " + response.status);
-    }
-    return response.json()
-  })
-}
-
-export function getAllWorkersShiftsByDates(startDate, endDate) {
-  const params = new URLSearchParams({startDate, endDate});
-  return fetch(`http://localhost:8088/shift/everyone?${params.toString()}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("HTTP status " + response.status);
-      }
-      return response.json();
+      return response.data;
     });
 }
 
+export function getShiftById(token, shiftId) {
+  const params = {token, shiftId};
+  return axios.get(`http://localhost:8088/shift`, {params})
+    .then((response) => {
+      return response.data;
+    });
+}
+
+export function addShift(token, date, startHour, endHour, isAvailable, workerId) {
+  const data = {token, date, startHour, endHour, isAvailable, workerId};
+  return axios.post('http://localhost:8088/shift', data)
+    .then((response) => {
+      return response.data;
+    });
+}
+
+export function updateWorkerShift(token, startHour, endHour, isAvailable, shiftId, workerId) {
+  const data = {startHour, endHour, isAvailable, workerId};
+  return axios.put(`http://localhost:8088/shift/${shiftId}`, data, {headers: {'Authorization': `Bearer ${token}`} })
+    .then((response) => {
+      return response.data;
+    });
+}
+
+export function deleteWorkerShift(token, shiftId, workerId) {
+  const data = {shiftId, workerId};
+  return axios.delete(`http://localhost:8088/shift/${shiftId}`, {headers: {'Authorization': `Bearer ${token}`}, data})
+    .then((response) => {
+      return response.data;
+    });
+}
+
+export function getAllShifts() {
+  return axios.get('http://localhost:8088/shift/all')
+    .then((response) => {
+      return response.data;
+    });
+}
+
+export function getAllWorkersShiftsByDates(startDate, endDate) {
+  const params = {startDate, endDate};
+  return axios.get(`http://localhost:8088/shift/everyone`, {params})
+    .then((response) => {
+      return response.data;
+    });
+}
 
 export function getWorkerShift(startDate, endDate, workerId) {
-  const params = new URLSearchParams({startDate, endDate, workerId});
-  return fetch(`https://localhost:8088/shift/worker?${params.toString()}`)
+  const params = {startDate, endDate, workerId};
+  return axios.get(`http://localhost:8088/shift/worker`, {params})
     .then((response) => {
-      if(!response.ok) {
-        throw new Error("HTTP status " + response.status);
-      }
-      return response.json();
+      return response.data;
     });
 }
 
 export function getWorker(token, workerId) {
-  const params = new URLSearchParams({token, workerId});
-  return fetch(`https://localhost:8088/workers?${token, workerId}`)
+  const params = {token, workerId};
+  return axios.get(`http://localhost:8088/workers`, {params})
     .then((response) => {
-      if(!response.ok) {
-        throw new Error("HTTP status " + response.status)
-      }
-      return response.json();
+      return response.data;
     });
 }
 
 export function addWorker(token, name, surname, password, username, workerLevel) {
-  const data = new FormData();
-  data.append('token', token);
-  data.append('name', name);
-  data.append('surname', surname);
-  data.append('password', password);
-  data.append('username', username);
-  data.append('workerLevel', workerLevel);
-
-  return fetch(`http://localhost:8088/workers/`, {
-    method: 'POST',
-    body: data
-  })
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("HTTP status " + response.status);
-    }
-    return response.json();
-  });
+  const data = {token, name, surname, password, username, workerLevel};
+  return axios.post('http://localhost:8088/workers', data)
+    .then((response) => {
+      return response.data;
+    });
 }
-
 
 export function updateWorker(token, workerId, name, surname, password, username, workerLevel) {
-  const data = new FormData();
-  data.append('token', token);
-  data.append('name', name);
-  data.append('surname', surname);
-  data.append('password', password);
-  data.append('username', username);
-  data.append('workerLevel', workerLevel);
-
-  return fetch(`http://localhost:8088/workers/${workerId}`, {
-    method: 'PUT',
-    body: data
-  })
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("HTTP status " + response.status);
-    }
-    return response.json();
-  });
-}
-
-
-export function deleteWorker(token, workerId) {
-  const data = new FormData();
-  data.append('token', token);
-  data.append('workerId', workerId);
-
-  return fetch(`http://localhost:8088/workers`, {
-    method: 'DELETE',
-    body: data
-  })
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("HTTP status " + response.status);
-    }
-    return response.json();
-  });
-}
-
-
-export function getAllWorkers(token) {
-  const params = new URLSearchParams({token});
-  return fetch(`https://localhost:8088/workers/all?${params}`)
+  const data = {token, name, surname, password, username, workerLevel};
+  return axios.put(`http://localhost:8088/workers/${workerId}`, data)
     .then((response) => {
-      if(!response.ok) {
-        throw new Error("HTTP status " + response.status + " Message: " + response.text())
-      }
-      return response.json();
+      return response.data;
     });
 }
